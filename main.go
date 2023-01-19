ca281a8510c45e41b534951da335c4cad072a76b
PhoenixSmaug <account.it.github@cmuessig.de>
2023-01-19 22:41:22 +0100

    cover, uncover, MRV heuristic

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 func main() {
 	m := initialize(3, 4)
 
-	t := []int{1, 2, 4, 6}
+	addRow(m, []int{1, 2, 4, 6})
+	addRow(m, []int{3, 5})
+	addRow(m, []int{2, 5})
 
-	addRow(m, t)
-	addRow(m, t)
+	c := &m.columns[2]
+
+	cover(c)
+
+	uncover(c)
 
 	fmt.Println(m.numCols)
 }
