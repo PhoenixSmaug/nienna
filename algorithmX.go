0c561ce602d930034bce28fd9bcd06cee44fb9f6
PhoenixSmaug <account@miamuessig.de>
2023-01-19 22:41:22 +0100

    cover, uncover, MRV heuristic

--- algorithmX.go
+++ algorithmX.go
@@ -82,6 +82,8 @@ func addRow(m *matrix, indices []int) {
 		// insert new node in last row of column
 		current := node{value: m.numRows, col: &m.columns[e], down: &m.columns[e].head, up: m.columns[e].head.up}
 		current.debug = &current // TODO
+
+		m.columns[e].head.up.down = &current
 		m.columns[e].head.up = &current
 
 		m.columns[e].length++
@@ -92,4 +94,70 @@ func addRow(m *matrix, indices []int) {
 		m.columns[e].head.up.right = m.columns[indices[(i+1)%len(indices)]].head.up
 		m.columns[indices[(i+1)%len(indices)]].head.up.left = m.columns[e].head.up
 	}
+}
+
+func cover(c *column) {
+	// remove from header row
+	c.left.right = c.right
+	c.right.left = c.left
+
+	// cover all rows covered by column c
+	for row := c.head.down; row != &(c.head); row = row.down {
+		for e := row.right; e != row; e = e.right {
+			e.up.down = e.down
+			e.down.up = e.up
+
+			// decrease size of column
+			e.col.length--
+		}
+	}
+}
+
+func coverCol(n *node) {
+	// cover all other columns of selected row
+
+	for e := n.right; e != n; e = e.right {
+		cover(e.col)
+	}
+}
+
+func uncover(c *column) {
+	// uncover all rows covered by column c
+	for row := c.head.up; row != &(c.head); row = row.up {
+		for e := row.left; e != row; e = e.left {
+			e.up.down = e
+			e.down.up = e
+
+			e.col.length++
+		}
+	}
+
+	c.left.right = c
+	c.right.left = c
+}
+
+func uncoverCol(n *node) {
+	// cover all other columns of selected row
+
+	for e := n.left; e != n; e = e.left {
+		uncover(e.col)
+	}
+}
+
+func heuristic(m *matrix) *column {
+	// Knuths MRV Heuristic, choose column which is fulfilled by least number of rows
+
+	// start with column 1
+	minLen := m.columns[1].length
+	minCol := &m.columns[1]
+
+	// check if smaller column exists
+	for c := m.columns[1].right; c != &m.columns[0]; c = c.right {
+		if c.length < minLen {
+			minLen = c.length
+			minCol = c
+		}
+	}
+
+	return minCol
 }
