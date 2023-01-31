9ef01a4a0a6c231b4da4ec3e7aadee24faa38fc0
PhoenixSmaug <account.it.github@cmuessig.de>
2023-01-31 21:54:47 +0100

    readme algorithm x, examples

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Nienna
+
+This repository is still in early development and is subject to heavy change.
+
+## Algorithm X
+
+Donald Knuth describes his algorithm in detail [here](https://arxiv.org/abs/cs/0011047). It finds all solutions of the Exact Cover Problem, where you have given a (0,1)-Matrix and need to find all subsets of rows which add to a vector with 1 in all entries.
+
+```math
+\begin{pmatrix}0 & 0 & 1 & 0 & 1 & 1 & 0\\1 & 0 & 0 & 1 & 0 & 0 & 1\\0 & 1 & 1 & 0 & 0 & 1 & 0\\ 1 & 0 & 0 & 1 & 0 & 0 & 0\\0 & 1 & 0 & 0 & 0 & 0 & 1\\0 & 0 & 0 & 1 & 1 & 0 & 1 \end{pmatrix}
+```
+
+For this exemplary matrix only one solution exists, consisting of the first, fourth and fifth row. This NP-complete problem can be extended with secondary columns, in whose entry the sum of the rows is no longer required to be exactly 1, but can also be 0.
+
+The following three combinatorial problems are implemented and have an example in main.go:
+
+* **n-Queens** with one or all solutions
+
+* **Sudoku** solver
+
+* Pack **Pentominoes** into a rectangle
+
+## Algorithm C
+
+## Algorithm M
--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"fmt"
 	algorithmX "nienna/Algorithm-X"
 )
 
 func main() {
-	// pentominoes
-	f := algorithmX.Polyomino{Tiles: [][]bool{{false, true, true}, {true, true, false}, {false, true, false}}}
+	// n Queens problem
+	algorithmX.PrintMaxQueens(60)
+	algorithmX.CountMaxQueens(13)
 
+	// Sudoku solving
+	mat := [9][9]int{{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 3, 0, 8, 5},
+		{0, 0, 1, 0, 2, 0, 0, 0, 0},
+		{0, 0, 0, 5, 0, 7, 0, 0, 0},
+		{0, 0, 4, 0, 0, 0, 1, 0, 0},
+		{0, 9, 0, 0, 0, 0, 0, 0, 0},
+		{5, 0, 0, 0, 0, 0, 0, 7, 3},
+		{0, 0, 2, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 4, 0, 0, 0, 9}}
+
+	s := algorithmX.Sudoku{Board: mat}
+
+	algorithmX.ShowSudoku(s)
+	algorithmX.SolveSudoku(&s)
+	algorithmX.ShowSudoku(s)
+
+	fmt.Println("")
+	fmt.Println("")
+
+	// Pentomino packing
+	f := algorithmX.Polyomino{Tiles: [][]bool{{false, true, true}, {true, true, false}, {false, true, false}}}
 	i := algorithmX.Polyomino{Tiles: [][]bool{{true}, {true}, {true}, {true}, {true}}}
 	l := algorithmX.Polyomino{Tiles: [][]bool{{true, false}, {true, false}, {true, false}, {true, true}}}
 	n := algorithmX.Polyomino{Tiles: [][]bool{{false, true}, {true, true}, {true, false}, {true, false}}}
@@ -22,5 +46,6 @@ func main() {
 
 	pentominoes := []algorithmX.Polyomino{f, i, l, n, p, t, u, v, w, x, y, z}
 
+	// pack the 12 pentominoes into the 10x6 rectangle
 	algorithmX.SolvePacking(pentominoes, 10, 6)
 }
