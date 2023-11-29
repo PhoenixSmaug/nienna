4a0f7290762d4a4a3d246e8ddbed12ac5bc842f8
PhoenixSmaug <account@miamuessig.de>
2023-11-29 12:31:52 +0100

    add mondrian example

--- Algorithm-X/mondrian.go
+++ Algorithm-X/mondrian.go
@@ -1,1 +1,149 @@
 package algorithmX
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func divisors(n int) []int {
+	var div []int
+
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			div = append(div, i)
+		}
+	}
+
+	return div
+}
+
+// return rounded int of p/q
+func truncDiv(p int, q int) int {
+	return int(math.Round(float64(p) / float64(q)))
+}
+
+func ceilDiv(p int, q int) int {
+	return int(math.Ceil(float64(p) / float64(q)))
+}
+
+/*
++-----------------------------------------------+--------------------+---------------------+
+|                       -                       | Tile covered (n^2) |  Rectangle used (m) |
++-----------------------------------------------+--------------------+---------------------+
+| Rectangle 1 (1, 1)                            |                    |                     |
+| ...                                           |                    |                     |
+| Rectangle 1 (n - sizeX(1), n - sizeY(1))      |                    |                     |
+| Rectangle 1 rot. (1, 1)                       |                    |                     |
+| ...                                           |                    |                     |
+| Rectangle 1 rot. (n - sizeY(1), n - sizeX(1)) |                    |                     |
+| ....                                          |                    |                     |
+| Rectangle m rot. (n - sizeY(m), n - sizeX(m)) |                    |                     |
++-----------------------------------------------+--------------------+---------------------+
+*/
+
+func rectangleMatrix(rectsX []int, rectsY []int, n int) *Matrix {
+	m := Initialize(n*n, len(rectsX))
+
+	for i, _ := range rectsX {
+		for _, rot := range [2]bool{true, false} {
+			sx := 0
+			sy := 0
+			if rot {
+				sx = rectsX[i]
+				sy = rectsY[i]
+			} else {
+				sx = rectsY[i]
+				sy = rectsX[i]
+			}
+
+			for px := 0; px <= n-sx; px++ {
+				for py := 0; py <= n-sy; py++ {
+					var row []int
+
+					for x := px + 1; x <= px+sx; x++ {
+						for y := py + 1; y <= py+sy; y++ {
+							row = append(row, x+(y-1)*n)
+						}
+					}
+
+					row = append(row, n*n+i+1) // i-th rectangle used
+					sort.Ints(row)
+					AddRow(m, row)
+				}
+			}
+		}
+	}
+
+	return m
+}
+
+func printRectangles(rows [][]bool, n int) {
+	var output [][]int32
+	for i := 0; i < n; i++ {
+		output = append(output, make([]int32, n))
+	}
+
+	for i := 0; i < len(rows); i++ {
+		for j := 0; j < n*n; j++ {
+			if rows[i][j] {
+				output[j/n][j%n] = rune('A' + i)
+			}
+		}
+	}
+
+	for i := 0; i < len(output); i++ {
+		for j := 0; j < len(output[0]); j++ {
+			fmt.Printf("%c", output[i][j])
+			fmt.Print(" ")
+		}
+		fmt.Println("")
+	}
+}
+
+func SolveMondrian(n int) {
+	minPieces := 4 //TODO 9 // proof in paper
+	d := divisors(n * n)
+
+	for _, r := range d {
+		if r >= minPieces {
+			area := truncDiv(n*n, r)
+			dA := divisors(area)
+
+			var s []int
+			for _, i := range dA {
+				if i <= n && area/i <= n {
+					s = append(s, i)
+				}
+			}
+
+			if ceilDiv(len(s), 2) < r {
+				continue
+			}
+
+			var rectsX []int
+			var rectsY []int
+			for i := 1; i <= ceilDiv(len(s), 2); i++ {
+				rectsX = append(rectsX, s[i-1])
+				rectsY = append(rectsY, truncDiv(area, s[i-1]))
+			}
+
+			m := rectangleMatrix(rectsX, rectsY, n)
+
+			fmt.Println("---------------------------------------------")
+			fmt.Println("n = " + strconv.Itoa(n) + ", r = " + strconv.Itoa(r))
+			fmt.Println(rectsX)
+			fmt.Println(rectsY)
+
+			start := time.Now()
+			FindFirst(m)
+			elapsed := time.Since(start)
+
+			fmt.Printf("Time: %s\n", elapsed)
+
+			fmt.Println("---------------------------------------------")
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,58 +1,63 @@
 package main
 
 import (
-	"fmt"
 	algorithmX "nienna/Algorithm-X"
 )
 
 func main() {
-	// Example: n Queens problem
+	algorithmX.SolveMondrian(224)
 
-	// Solve the n Queens problem on a 60x60 board
-	algorithmX.PrintMaxQueens(60)
+	//fmt.Println(algorithmX.Divisors(20))
 
-	// Count the number of solutions to the n Queens problem on a 13x13 board
-	algorithmX.CountMaxQueens(13)
+	/*
+		// Example: n Queens problem
 
-	// Example: Sudoku solving
+		// Solve the n Queens problem on a 60x60 board
+		algorithmX.PrintMaxQueens(60)
 
-	// example sudoku
-	mat := [9][9]int{{0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 3, 0, 8, 5},
-		{0, 0, 1, 0, 2, 0, 0, 0, 0},
-		{0, 0, 0, 5, 0, 7, 0, 0, 0},
-		{0, 0, 4, 0, 0, 0, 1, 0, 0},
-		{0, 9, 0, 0, 0, 0, 0, 0, 0},
-		{5, 0, 0, 0, 0, 0, 0, 7, 3},
-		{0, 0, 2, 0, 1, 0, 0, 0, 0},
-		{0, 0, 0, 0, 4, 0, 0, 0, 9}}
-	s := algorithmX.Sudoku{Board: mat}
+		// Count the number of solutions to the n Queens problem on a 13x13 board
+		algorithmX.CountMaxQueens(13)
 
-	// Solve the sudoku s
-	algorithmX.ShowSudoku(s)
-	algorithmX.SolveSudoku(&s)
-	algorithmX.ShowSudoku(s)
+		// Example: Sudoku solving
 
-	fmt.Println("")
+		// example sudoku
+		mat := [9][9]int{{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 3, 0, 8, 5},
+			{0, 0, 1, 0, 2, 0, 0, 0, 0},
+			{0, 0, 0, 5, 0, 7, 0, 0, 0},
+			{0, 0, 4, 0, 0, 0, 1, 0, 0},
+			{0, 9, 0, 0, 0, 0, 0, 0, 0},
+			{5, 0, 0, 0, 0, 0, 0, 7, 3},
+			{0, 0, 2, 0, 1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 4, 0, 0, 0, 9}}
+		s := algorithmX.Sudoku{Board: mat}
 
-	// Example: Polyomino packing
+		// Solve the sudoku s
+		algorithmX.ShowSudoku(s)
+		algorithmX.SolveSudoku(&s)
+		algorithmX.ShowSudoku(s)
 
-	// the 12 pentominoes as example
-	f := algorithmX.Polyomino{Tiles: [][]bool{{false, true, true}, {true, true, false}, {false, true, false}}}
-	i := algorithmX.Polyomino{Tiles: [][]bool{{true}, {true}, {true}, {true}, {true}}}
-	l := algorithmX.Polyomino{Tiles: [][]bool{{true, false}, {true, false}, {true, false}, {true, true}}}
-	n := algorithmX.Polyomino{Tiles: [][]bool{{false, true}, {true, true}, {true, false}, {true, false}}}
-	p := algorithmX.Polyomino{Tiles: [][]bool{{true, true}, {true, true}, {true, false}}}
-	t := algorithmX.Polyomino{Tiles: [][]bool{{true, true, true}, {false, true, false}, {false, true, false}}}
-	u := algorithmX.Polyomino{Tiles: [][]bool{{true, false, true}, {true, true, true}}}
-	v := algorithmX.Polyomino{Tiles: [][]bool{{true, false, false}, {true, false, false}, {true, true, true}}}
-	w := algorithmX.Polyomino{Tiles: [][]bool{{true, false, false}, {true, true, false}, {false, true, true}}}
-	x := algorithmX.Polyomino{Tiles: [][]bool{{false, true, false}, {true, true, true}, {false, true, false}}}
-	y := algorithmX.Polyomino{Tiles: [][]bool{{false, true}, {true, true}, {false, true}, {false, true}}}
-	z := algorithmX.Polyomino{Tiles: [][]bool{{true, true, false}, {false, true, false}, {false, true, true}}}
+		fmt.Println("")
 
-	pentominoes := []algorithmX.Polyomino{f, i, l, n, p, t, u, v, w, x, y, z}
+		// Example: Polyomino packing
 
-	// Find a way to pack the pentominoes into the 10x6 rectangle
-	algorithmX.SolvePacking(pentominoes, 10, 6)
+		// the 12 pentominoes as example
+		f := algorithmX.Polyomino{Tiles: [][]bool{{false, true, true}, {true, true, false}, {false, true, false}}}
+		i := algorithmX.Polyomino{Tiles: [][]bool{{true}, {true}, {true}, {true}, {true}}}
+		l := algorithmX.Polyomino{Tiles: [][]bool{{true, false}, {true, false}, {true, false}, {true, true}}}
+		n := algorithmX.Polyomino{Tiles: [][]bool{{false, true}, {true, true}, {true, false}, {true, false}}}
+		p := algorithmX.Polyomino{Tiles: [][]bool{{true, true}, {true, true}, {true, false}}}
+		t := algorithmX.Polyomino{Tiles: [][]bool{{true, true, true}, {false, true, false}, {false, true, false}}}
+		u := algorithmX.Polyomino{Tiles: [][]bool{{true, false, true}, {true, true, true}}}
+		v := algorithmX.Polyomino{Tiles: [][]bool{{true, false, false}, {true, false, false}, {true, true, true}}}
+		w := algorithmX.Polyomino{Tiles: [][]bool{{true, false, false}, {true, true, false}, {false, true, true}}}
+		x := algorithmX.Polyomino{Tiles: [][]bool{{false, true, false}, {true, true, true}, {false, true, false}}}
+		y := algorithmX.Polyomino{Tiles: [][]bool{{false, true}, {true, true}, {false, true}, {false, true}}}
+		z := algorithmX.Polyomino{Tiles: [][]bool{{true, true, false}, {false, true, false}, {false, true, true}}}
+
+		pentominoes := []algorithmX.Polyomino{f, i, l, n, p, t, u, v, w, x, y, z}
+
+		// Find a way to pack the pentominoes into the 10x6 rectangle
+		algorithmX.SolvePacking(pentominoes, 10, 6)
+	*/
 }
